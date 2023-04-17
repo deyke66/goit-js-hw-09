d9cb8762d82b9b854d79bfac62e66e026044888e
Kurnosov Valentyn <deyke66@gmail.com>
2023-04-17 21:13:28 +0200

    task-3-finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay)
+  });
 }
+
+
+const refs = {
+  promisesForm: document.querySelector('.form'),
+  inputDelay: document.querySelector('input[name="delay"]'),
+  inputStep: document.querySelector('input[name="step"]'),
+  inputAmount: document.querySelector('input[name="amount"]'),
+
+}
+
+const { promisesForm, inputAmount, inputDelay, inputStep } = refs;
+
+promisesForm.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const quantityPromises = inputAmount.value;
+  let delayValue = Number(inputDelay.value);
+
+  for (let i = 1; i <= quantityPromises; i++) {
+    createPromise(i, delayValue).then(value => Notiflix.Notify.success(value)).catch(err => Notiflix.Notify.failure(err));
+    delayValue += Number(inputStep.value);
+    console.log(inputStep.value)
+    }
+  
+}
+
+
